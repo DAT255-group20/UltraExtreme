7d844ee2a5309ff7529f13276e6dd705bf6a6039
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-09-15 23:09:34 +0200

    Added test class for BulletProductionQueue

---
+++ test/ultraextreme/model/BulletProductionQueueTest.java
@@ -0,0 +1,22 @@
+package ultraextreme.model;
+
+import junit.framework.TestCase;
+
+public class BulletProductionQueueTest extends TestCase {
+
+	public void testAddBullet() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetNewBullets() {
+		fail("Not yet implemented");
+	}
+
+	public void testDropBomb() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsBombDropped() {
+		fail("Not yet implemented");
+	}
+}
