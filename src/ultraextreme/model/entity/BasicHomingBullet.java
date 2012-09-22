edeaf85aa3201983300826406cb57ce5b2424744
Viktor Anderling <natanande@gmail.com>
2012-09-22 15:03:01 +0200

    Made BasicHomingBullet more efficient

--- src/ultraextreme/model/entity/BasicHomingBullet.java
+++ src/ultraextreme/model/entity/BasicHomingBullet.java
@@ -1,51 +1,38 @@
 package ultraextreme.model.entity;
 
+import javax.vecmath.Vector2d;
+
 import ultraextreme.model.util.Direction;
 import ultraextreme.model.util.PlayerID;
 import ultraextreme.model.util.Position;
 
 public class BasicHomingBullet extends AbstractHomingBullet {
+	
+	private Vector2d vector;
 
 	public BasicHomingBullet(double x, double y, int width, int height,
 			PlayerID playerId, DestroyableEntity target) {
 		super(x, y, width, height, playerId, Direction.UP);
 		this.setTarget(target);
+		vector = new Vector2d();
+		updateDirection();
+		
 	}
 
-	@Override
-	public void doMovement(float timePassed) {
-		//TODO Test before optimizing code!
+	private void updateDirection() {
 		Position targetPosition = target.getPosition();
 		Position thisPosition = this.getPosition();
-		double deltaX = targetPosition.getX() - thisPosition.getX();
-		double deltaY = targetPosition.getY() - thisPosition.getY();
-		double moveX = 0;
-		double moveY = 0;
-		double speedMod = this.getSpeedMod();
-		
-		if(deltaX * deltaY == 0) {
-			if(deltaX + deltaY == 0) {
-				moveX = 0;
-				moveY = 0;
-			} else if(deltaX == 0) {
-				moveX = 0;
-				moveY = timePassed * this.getSpeedMod();
-			} else if(deltaY == 0) {
-				moveY = 0;
-				moveX = timePassed * this.getSpeedMod();
-			}
-		} else {
-			double k = Math.abs(Math.min(deltaY, deltaX) / Math.max(deltaY, deltaX));
-			if(deltaY < deltaX) { 
-				moveX = 1;
-				moveY = k;
-			} else {
-				moveY = 1;
-				moveX = k;
-			}	
+		vector.normalize(new Vector2d(targetPosition.getX() - thisPosition.getX(), 
+				targetPosition.getY() - thisPosition.getY()));
+	}
+	
+	@Override
+	public void doMovement(float timePassed) {
+		if(!((DestroyableEntity)target).isDestroyed()) {
+			updateDirection();
 		}
-		this.move(moveX * Math.signum(deltaX) * timePassed * speedMod, 
-				moveY * Math.signum(deltaY) * timePassed * speedMod);
+		this.move(vector.x * timePassed * this.getSpeedMod(), 
+				vector.y * timePassed * this.getSpeedMod());
 	}
 
 }
