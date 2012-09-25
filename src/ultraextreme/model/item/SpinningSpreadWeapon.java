2bb6b09a6405ace84f1b73a2375e452f50b96849
Viktor Anderling <natanande@gmail.com>
2012-09-25 12:01:57 +0200

    Added basic code to spinningweapon

--- src/ultraextreme/model/item/SpinningSpreadWeapon.java
+++ src/ultraextreme/model/item/SpinningSpreadWeapon.java
@@ -1,5 +1,20 @@
 package ultraextreme.model.item;
 
-public class SpinningSpreadWeapon {
+import ultraextreme.model.util.Direction;
+import ultraextreme.model.util.PlayerID;
+import ultraextreme.model.util.Position;
+
+public class SpinningSpreadWeapon extends AbstractWeapon {
+
+	public SpinningSpreadWeapon(BulletManager bulletManager) {
+		super(bulletManager);
+	}
+
+	@Override
+	public void fireShot(Position shipPosition, PlayerID playerId,
+			Direction direction) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
