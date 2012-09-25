f4aa6f34a2fe26010a792e0d6a3abd6f34c169c6
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-09-25 16:25:36 +0200

    Created a ControllerEventTest

---
+++ tests/UltraExtremeTest/src/ultraextreme/controller/ControllerEventTest.java
@@ -0,0 +1,35 @@
+package ultraextreme.controller;
+
+import static org.junit.Assert.*;
+
+import org.andengine.entity.scene.Scene;
+import org.junit.Test;
+
+import ultraextreme.controller.ControllerEvent.ControllerEventType;
+
+public class ControllerEventTest {
+
+	@Test
+	public void testControllerEvent() {
+		AbstractController testController = new AbstractController() {
+			
+			@Override
+			public Scene getScene() {
+				return null;
+			}
+		};
+		ControllerEvent event = new ControllerEvent(null, null);
+		assertNull(event.getSource());
+		assertNull(event.getEventType());
+		
+		event = new ControllerEvent(testController, ControllerEventType.SWITCH_TO_GAME);
+		assertEquals(testController, event.getSource());
+		assertEquals(ControllerEventType.SWITCH_TO_GAME, event.getEventType());
+		assertNotSame(ControllerEventType.SWITCH_TO_HIGHSCORE, event.getEventType());
+		
+		event = new ControllerEvent(null, ControllerEventType.SWITCH_TO_MENU);
+		assertNull(event.getSource());
+		assertEquals(ControllerEventType.SWITCH_TO_MENU, event.getEventType());
+		assertNotSame(ControllerEventType.SWITCH_TO_GAME, event.getEventType());
+	}
+}
