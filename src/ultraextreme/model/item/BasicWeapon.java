88e7ee0fc03c7a639a98651f95c9f583d2c04c65
Viktor Anderling <natanande@gmail.com>
2012-09-25 19:35:41 +0200

    changed firing speed of BasicWeapon

--- src/ultraextreme/model/item/BasicWeapon.java
+++ src/ultraextreme/model/item/BasicWeapon.java
@@ -9,23 +9,27 @@ public class BasicWeapon extends AbstractWeapon {
 
 	private static final int bulletWidth = 5;
 	private static final int bulletHeight = 5;
-	private static double initCooldown = 1;
+	private static double initCooldown = 0.5;
 	private double cooldown;
 	
 	public BasicWeapon(BulletManager bulletManager) {
 		super(bulletManager);
-		cooldown = 0;
+		cooldown = initCooldown;
 	}
 
 	@Override
 	public void fire(Position shipPosition, PlayerID playerId,
 			Rotation direction, float timeElapsed) {
+		cooldown = cooldown - timeElapsed;
 		if(cooldown < 0) {
 			cooldown = cooldown + initCooldown;
 			this.getBulletManager().addBullet(
 					new BasicBullet(shipPosition.getX(), shipPosition.getY(), 
 							bulletWidth, bulletHeight, playerId, direction));
 		}
-		cooldown = cooldown - timeElapsed;
+	}
+	
+	public double getInitCooldown() {
+		return initCooldown;
 	}
 }
