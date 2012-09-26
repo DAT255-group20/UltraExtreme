8d2550f25d7a91435dc26edf0ed48e8f0334f52d
Viktor Anderling <natanande@gmail.com>
2012-09-26 20:30:57 +0200

    Added functions including scaling to Dimension

--- src/ultraextreme/model/util/Dimension.java
+++ src/ultraextreme/model/util/Dimension.java
@@ -1,5 +1,7 @@
 package ultraextreme.model.util;
 
+import javax.vecmath.Vector2d;
+
 /**
  * Defines an immutable positive dimension with an x-value and an y-value with
  * decimals.
@@ -21,8 +23,8 @@ public class Dimension {
 	 *            A positive double.
 	 */
 	public Dimension(double x, double y) {
-		if (x < 0 || y < 0) {
-			throw new IllegalArgumentException("Negative input value");
+		if (x <= 0 || y <= 0) {
+			throw new IllegalArgumentException("Non-positive input value");
 		} else {
 			this.xValue = x;
 			this.yValue = y;
@@ -41,5 +43,48 @@ public class Dimension {
 	 */
 	public double getY() {
 		return yValue;
+	}
+	
+	/**
+	 * Calculates the quotient between this and the other dimension
+	 * 
+	 * @param otherDimension
+	 * @return A vector with an x-quotient and an y-quotient.
+	 */
+	public Vector2d getQuotient(Dimension otherDimension) {
+		return new Vector2d(this.getX()/otherDimension.getX(), 
+				this.getY()/otherDimension.getY());
+	}
+	
+	/**
+	 * Scales the given position in the given dimension to 
+	 * a position of this dimension.
+	 * 
+	 * @param otherDimension
+	 * 					The dimension of the other position.
+	 * @param otherPosition
+	 * 					The other position that will be scaled.
+	 * @return A position scaled from the other position.
+	 */
+	public Position scalePosition(Dimension otherDimension, Position otherPosition) {
+		if(this.equals(otherDimension)) {
+			return otherPosition;
+		} else {
+			Vector2d quotientVector = this.getQuotient(otherDimension);
+			return new Position(quotientVector.x * otherPosition.getX(), 
+					quotientVector.y * otherPosition.getY());
+		}	
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) {
+			return true;
+		} else if(this.getClass() != o.getClass()) {
+			return false;
+		} else {
+			return this.getX() == ((Dimension)o).getX() && 
+					this.getY() == ((Dimension)o).getY();
+		}
 	}
 }
