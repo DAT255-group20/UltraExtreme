a36ba1838e6c86a69f16a297ddc689ed33216a31
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-09-27 16:35:12 +0200

    Added a BulletManagerTest class

---
+++ tests/UltraExtremeTest/src/ultraextreme/model/item/BulletManagerTest.java
@@ -0,0 +1,64 @@
+package ultraextreme.model.item;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BulletManagerTest {
+
+	@Test
+	public void testBulletManager() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddBullet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBullets() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBulletsFrom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearBulletsOffScreen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearAllBullets() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClearAllBulletsFrom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDropBomb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsBombDropped() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPropertyChangeListener() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveListener() {
+		fail("Not yet implemented");
+	}
+
+}
