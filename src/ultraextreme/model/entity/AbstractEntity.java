d261dda5f4a387be5a2497ada211cded2a08c26a
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-09-27 15:23:38 +0200

    Optimized collision code

--- src/ultraextreme/model/entity/AbstractEntity.java
+++ src/ultraextreme/model/entity/AbstractEntity.java
@@ -106,20 +106,7 @@ public abstract class AbstractEntity {
 		double right2 = other.getPosition().getX()+other.getWidth();
 		double bottom2 = other.getPosition().getY()+other.getHeight();
 		
-		if (bottom1 < top2)
-		{
-			return false;
-		}
-		if (top1 > bottom2)
-		{
-			return false;
-		}
-		
-		if (right1 < left2)
-		{
-			return false;
-		}
-		if (left1 > right2)
+		if (bottom1 < top2 || top1 > bottom2 || right1 < left2 || left1 > right2)
 		{
 			return false;
 		}
