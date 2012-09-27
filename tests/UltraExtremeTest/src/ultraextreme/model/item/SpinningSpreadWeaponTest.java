0e24544db8169c9a9c970b99671cb07a7e4ae389
Viktor Anderling <natanande@gmail.com>
2012-09-27 15:26:31 +0200

    moved tests and created blank test for SpinningSpreadWeapon

---
+++ tests/UltraExtremeTest/src/ultraextreme/model/item/SpinningSpreadWeaponTest.java
@@ -0,0 +1,29 @@
+package ultraextreme.model.item;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SpinningSpreadWeaponTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFire() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSpinningSpreadWeapon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInitCooldown() {
+		fail("Not yet implemented");
+	}
+
+}
