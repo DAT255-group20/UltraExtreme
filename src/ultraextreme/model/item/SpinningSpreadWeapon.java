1f0c569bfc500fc086c3256aefb82455372a5205
Viktor Anderling <natanande@gmail.com>
2012-09-27 15:46:36 +0200

    Changed size of bullets

--- src/ultraextreme/model/item/SpinningSpreadWeapon.java
+++ src/ultraextreme/model/item/SpinningSpreadWeapon.java
@@ -8,8 +8,8 @@ import ultraextreme.model.util.Position;
 
 public class SpinningSpreadWeapon extends AbstractWeapon {
 
-	private static final int bulletWidth = 5;
-	private static final int bulletHeight = 5;
+	private static final int bulletWidth = 20;
+	private static final int bulletHeight = 40;
 	private static float initCooldown = 1/6f;
 	private float cooldown;
 	private static double angleStep;
