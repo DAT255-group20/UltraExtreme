867261a0a586753c3743adb91fd98780b610e5b2
MaTachi <dannie92@gmail.com>
2012-09-30 17:58:49 +0200

    The is now updated when an item in the model is picked up.

---
+++ src/ultraextreme/model/item/ItemBarUpdatedListener.java
@@ -0,0 +1,6 @@
+package ultraextreme.model.item;
+
+public interface ItemBarUpdatedListener {
+
+	public void updatedItemBar(ItemBar itemBar);
+}
--- src/ultraextreme/view/ItemBarPanel.java
+++ src/ultraextreme/view/ItemBarPanel.java
@@ -1,19 +1,16 @@
 package ultraextreme.view;
 
 import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
 import ultraextreme.model.item.AbstractWeapon;
 import ultraextreme.model.item.ItemBar;
-import ultraextreme.model.util.ObjectName;
+import ultraextreme.model.item.ItemBarUpdatedListener;
 import ultraextreme.model.util.Position;
 
-public class ItemBarPanel extends Sprite implements PropertyChangeListener {
-
-	private ItemBar itemBar;
+public class ItemBarPanel extends Sprite implements ItemBarUpdatedListener {
 
 	private SpriteFactory spriteFactory;
 
@@ -25,24 +22,15 @@ public class ItemBarPanel extends Sprite implements PropertyChangeListener {
 		super((float) position.getX() * scaling, (float) position.getY()
 				* scaling, 750 * scaling, 75 * scaling, spriteFactory
 				.getItemBarTexture(), vertexBufferObjectManager);
-		this.itemBar = itemBar;
 		this.spriteFactory = spriteFactory;
 		this.vertexBufferObjectManager = vertexBufferObjectManager;
-		updateItemBar();
-//		this.attachChild(new Sprite(10, 10, spriteFactory
-//				.getItemTexture(ObjectName.BASIC_WEAPON),
-//				vertexBufferObjectManager));
-	}
-
-	@Override
-	public void propertyChange(PropertyChangeEvent event) {
-
+		updateItemBar(itemBar);
 	}
 
 	/**
 	 * Make the item bar update itself.
 	 */
-	public void updateItemBar() {
+	public void updateItemBar(ItemBar itemBar) {
 		// Clean the item bar from sprites
 		this.detachChildren();
 		// Repopulate it with sprites
@@ -53,6 +41,11 @@ public class ItemBarPanel extends Sprite implements PropertyChangeListener {
 			this.attachChild(new Sprite(x, y, spriteFactory.getItemTexture(item
 					.getName()), vertexBufferObjectManager));
 		}
+	}
+
+	@Override
+	public void updatedItemBar(ItemBar itemBar) {
+		updateItemBar(itemBar);
 	}
 
 }
