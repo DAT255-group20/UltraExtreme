6fbca63d5a5b2e722c994db98b22b0f0e4fb4943
Viktor Anderling <natanande@gmail.com>
2012-10-11 13:56:55 +0200

    Added test template for spreadWeapon

---
+++ tests/UltraExtremeTest/src/ultraextreme/model/item/SpreadWeaponTest.java
@@ -0,0 +1,29 @@
+package ultraextreme.model.item;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SpreadWeaponTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFire() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShallowClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBasicSpreadWeapon() {
+		fail("Not yet implemented");
+	}
+
+}
