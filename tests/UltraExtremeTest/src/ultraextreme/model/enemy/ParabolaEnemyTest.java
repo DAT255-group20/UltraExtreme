5bd6eb4af02195fbb26f00c3bb09456094173076
Zap <johan.gronvall.92@gmail.com>
2012-10-12 14:01:51 +0200

    worked on parabolaEnemyTest

--- tests/UltraExtremeTest/src/ultraextreme/model/enemy/ParabolaEnemyTest.java
+++ tests/UltraExtremeTest/src/ultraextreme/model/enemy/ParabolaEnemyTest.java
@@ -8,12 +8,15 @@ public class ParabolaEnemyTest extends TestCase {
 	private ParabolaEnemy enemy;
 	
 	public void setUp() {
-		Position startPos = new Position();
-		Position endPos = new Position();
+		Position startPos = new Position(200, 0);
+		Position endPos = new Position(0, 150);
 		enemy = new ParabolaEnemy(startPos, endPos, ObjectName.BASIC_WEAPON);
 	}
 	
 	public void testUpdate() {
 		
+		for(int i = 0; i < 10000; i++) {
+			
+		}
 	}
 }
