d7eaa6dde3c8a30036eec80b599fe648ea367b49
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-10-13 16:37:32 +0200

    Fixed preliminary constructors for Highscore scene and controller

--- src/ultraextreme/controller/HighscoreController.java
+++ src/ultraextreme/controller/HighscoreController.java
@@ -1,19 +1,26 @@
 package ultraextreme.controller;
 
+import org.andengine.engine.camera.Camera;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.menu.MenuScene;
 import org.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
 import org.andengine.entity.scene.menu.item.IMenuItem;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
 import ultraextreme.view.HighscoreScene;
 
 public class HighscoreController extends AbstractController implements
 		IOnMenuItemClickListener {
 	private final HighscoreScene scene;
+	private HighscoreDBOpenHelper dbOpenHelper;
 
-	public HighscoreController() {
+	public HighscoreController(Camera camera, Font font,
+			VertexBufferObjectManager vbo, HighscoreDBOpenHelper dbOpenHelper) {
 		super();
-		this.scene = new HighscoreScene();
+		this.scene = new HighscoreScene(camera, font, vbo);
+		scene.setOnMenuItemClickListener(this);
+		this.dbOpenHelper = dbOpenHelper;
 	}
 
 	@Override
