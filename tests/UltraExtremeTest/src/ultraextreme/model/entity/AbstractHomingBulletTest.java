2ad51ff61cc396ff8fadf9eb23ead2520f5d7310
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-10-17 09:50:27 +0200

    Updated the tests for the entity package

---
+++ tests/UltraExtremeTest/src/ultraextreme/model/entity/AbstractHomingBulletTest.java
@@ -0,0 +1,11 @@
+package ultraextreme.model.entity;
+
+import junit.framework.TestCase;
+
+public class AbstractHomingBulletTest extends TestCase {
+
+	public void testSetTarget() {
+		fail("Not yet tested");
+	}
+
+}
