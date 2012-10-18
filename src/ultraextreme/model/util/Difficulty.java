58ce00cb05f85ac14735c098d5c9deda0274f27c
Viktor Anderling <natanande@gmail.com>
2012-10-19 00:11:27 +0200

    Added a difficulty enum and a Settings class for it

---
+++ src/ultraextreme/model/util/Difficulty.java
@@ -0,0 +1,11 @@
+package ultraextreme.model.util;
+
+/**
+ * A Simple enumerator describing the difficulty of the game.
+ * 
+ * @author Viktor Anderling
+ *
+ */
+public enum Difficulty {
+	EASY, NORMAL, HARD, ULTRAEXTREME;
+}
