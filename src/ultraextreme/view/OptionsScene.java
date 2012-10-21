efeed8e215b5ba295b6a718b633892600745be3f
MaTachi <dannie92@gmail.com>
2012-10-22 01:46:04 +0200

    Cleaned up OptionsScene a bit

--- src/ultraextreme/view/OptionsScene.java
+++ src/ultraextreme/view/OptionsScene.java
@@ -20,11 +20,7 @@ public class OptionsScene extends MenuScene {
 	public static final int CHANGE_DIFFICULTY = 0;
 	public static final int RESET_HIGH_SCORES = 1;
 	public static final int RETURN_TO_MAIN_MENU = 2;
-	
-	private IMenuItem difficultyButton;
-	private VertexBufferObjectManager vertexBufferObjectManager;
-	private Vector2d scaling;
-	float screenWidth;
+
 	private IMenuItem[] difficultyButtons;
 	private int currentButton;
 
@@ -37,14 +33,12 @@ public class OptionsScene extends MenuScene {
 		 * Add the background.
 		 */
 		final float screenWidth = camera.getWidth();
-		this.screenWidth = screenWidth;
 		final float screenHeight = camera.getHeight();
 		final SpriteBackground background = new SpriteBackground(new Sprite(0,
 				0, screenWidth, screenHeight,
 				SpriteFactory.getOptionsTexture(OptionsTexture.BACKGROUND),
 				vertexBufferObjectManager));
 		setBackground(background);
-		this.vertexBufferObjectManager = vertexBufferObjectManager;
 
 		/*
 		 * Scaling
@@ -55,38 +49,44 @@ public class OptionsScene extends MenuScene {
 		// for
 		Vector2d scaling = screenDimension
 				.getQuotient(new Dimension(800, 1280));
-		this.scaling = scaling;
 
 		/*
 		 * Add the difficulty button.
 		 */
 		currentButton = 0;
 		difficultyButtons = new IMenuItem[4];
-		difficultyButtons[0] = createButton(CHANGE_DIFFICULTY, 350, OptionsTexture.NORMAL_DIFFICULTY,
-				scaling, screenWidth, vertexBufferObjectManager);
-		addMenuItem(difficultyButtons[0]);
-		difficultyButtons[1] = createButton(CHANGE_DIFFICULTY, 350, OptionsTexture.HARD_DIFFICULTY,
-				scaling, screenWidth, vertexBufferObjectManager);
-		difficultyButtons[2] = createButton(CHANGE_DIFFICULTY, 350, OptionsTexture.EXTREME_DIFFICULTY,
-				scaling, screenWidth, vertexBufferObjectManager);
-		difficultyButtons[3] = createButton(CHANGE_DIFFICULTY, 350, OptionsTexture.ULTRAEXTREME_DIFFICULTY,
-				scaling, screenWidth, vertexBufferObjectManager);
+		difficultyButtons[0] = createButton(CHANGE_DIFFICULTY, 350,
+				OptionsTexture.NORMAL_DIFFICULTY, scaling, screenWidth,
+				vertexBufferObjectManager);
+		difficultyButtons[1] = createButton(CHANGE_DIFFICULTY, 350,
+				OptionsTexture.HARD_DIFFICULTY, scaling, screenWidth,
+				vertexBufferObjectManager);
+		difficultyButtons[2] = createButton(CHANGE_DIFFICULTY, 350,
+				OptionsTexture.EXTREME_DIFFICULTY, scaling, screenWidth,
+				vertexBufferObjectManager);
+		difficultyButtons[3] = createButton(CHANGE_DIFFICULTY, 350,
+				OptionsTexture.ULTRAEXTREME_DIFFICULTY, scaling, screenWidth,
+				vertexBufferObjectManager);
+		addMenuItem(difficultyButtons[currentButton]);
 
 		/*
 		 * Add the reset button.
 		 */
-		addMenuItem(createButton(RESET_HIGH_SCORES, 650, OptionsTexture.RESET_BUTTON,
-				scaling, screenWidth, vertexBufferObjectManager));
-		
+		addMenuItem(createButton(RESET_HIGH_SCORES, 650,
+				OptionsTexture.RESET_BUTTON, scaling, screenWidth,
+				vertexBufferObjectManager));
+
 		/*
 		 * Add the return button.
 		 */
-		addMenuItem(createButton(RETURN_TO_MAIN_MENU, 950, OptionsTexture.RETURN_BUTTON,
-				scaling, screenWidth, vertexBufferObjectManager));
+		addMenuItem(createButton(RETURN_TO_MAIN_MENU, 950,
+				OptionsTexture.RETURN_BUTTON, scaling, screenWidth,
+				vertexBufferObjectManager));
 	}
 
 	/**
-	 * Add a button the the scene
+	 * Create a menu button.
+	 * 
 	 * @param destination
 	 * @param y
 	 * @param texture
@@ -109,15 +109,6 @@ public class OptionsScene extends MenuScene {
 	}
 
 	public void updateDifficultyButton(Difficulty difficulty) {
-//		detachChild(button);
-//		button = new SpriteMenuItem(CHANGE_DIFFICULTY,
-//				SpriteFactory.getOptionsTexture(OptionsTexture.EXTREME_DIFFICULTY),
-//				vertexBufferObjectManager);
-//		button.setWidth((float) scaling.x * button.getWidth());
-//		button.setHeight((float) scaling.y * button.getHeight());
-//		button.setX((screenWidth - button.getWidth()) / 2);
-//		button.setY((float) scaling.y * 350);
-//		addMenuItem(button);
 		detachChild(difficultyButtons[currentButton++]);
 		currentButton %= 4;
 		addMenuItem(difficultyButtons[currentButton]);
